75980e80a90d83e4fed0e18e1a7a811f065277d6
Việt Thảo <t.to.the.rekt@gmail.com>
2018-09-25 17:32:38 +0700

    Added environment config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,25 @@
 import * as firebase from 'firebase';
 
-let config = {
-  apiKey: "AIzaSyBubaiMXyO0Y_XQTwWN6isJ1lzsfCE60xY",
-  authDomain: "techkids-police.firebaseapp.com",
-  databaseURL: "https://techkids-police.firebaseio.com",
-  projectId: "techkids-police",
-  storageBucket: "techkids-police.appspot.com",
-  messagingSenderId: "908532066309"
+var config;
+
+if (process.env.NODE_ENV === 'development') {
+  config = {
+    apiKey: "AIzaSyAJREnQb0EnCtxIq0nb3S86NKzcbqmOlWs",
+    authDomain: "techkids-police-test.firebaseapp.com",
+    databaseURL: "https://techkids-police-test.firebaseio.com",
+    projectId: "techkids-police-test",
+    storageBucket: "techkids-police-test.appspot.com",
+    messagingSenderId: "759129514657"
+  };
+} else {
+  config = {
+    apiKey: "AIzaSyBubaiMXyO0Y_XQTwWN6isJ1lzsfCE60xY",
+    authDomain: "techkids-police.firebaseapp.com",
+    databaseURL: "https://techkids-police.firebaseio.com",
+    projectId: "techkids-police",
+    storageBucket: "techkids-police.appspot.com",
+    messagingSenderId: "908532066309"
+  }
 }
 
 firebase.initializeApp(config);
