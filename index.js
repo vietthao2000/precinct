d4db9e4bfe632eae8b2f98c0fe16f38e9abf0c99
Việt Thảo <t.to.the.rekt@gmail.com>
2018-09-25 17:48:32 +0700

    Comment out unused dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+
+app.use('/', express.static(__dirname + '/build'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+})
