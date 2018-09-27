9790a8b471d0bbbb69b94812ee5be0350acb5b33
Việt Thảo <t.to.the.rekt@gmail.com>
2018-09-27 17:55:35 +0700

    Built files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 let PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/build'));
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
